36745fd2a436fe015f013b2f44976d247cb2905e
Deep Panchani <dlplearning00@gmail.com>
2021-07-04 14:28:11 +0530

    isset() condition ➕

--- check.php
+++ check.php
@@ -1,11 +1,15 @@
 <?php
-    
-    $fname = $_POST['fname'];
-    $email = $_POST['email'];
-    $pnumber = $_POST['pnumber'];
-    $password = $_POST['password'];
+    if(isset($_POST['submit'])){
+        if(isset($_POST['fname']) &&  isset($_POST['email']) &&  isset($_POST['pnumber']) && isset($_POST['password'])){
+            $fname = $_POST['fname'];
+            $email = $_POST['email'];
+            $pnumber = $_POST['pnumber'];
+            $password = $_POST['password'];
+        
+            $event[] = $_POST['events'];
+            $activity[] = $_POST['activity'];
+        }
+    }
 
-    $event[] = $_POST['events'];
-    $activity[] = $_POST['activity'];
 
 ?>
