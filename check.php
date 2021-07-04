af95efb22053b9335abe43292230591beebbaaf7
Deep Panchani <dlplearning00@gmail.com>
2021-07-04 14:52:31 +0530

    Update check.php

--- check.php
+++ check.php
@@ -1,13 +1,20 @@
 <?php
     if(isset($_POST['submit'])){
-        if( isset($_POST['fname']) &&  isset($_POST['email']) &&  isset($_POST['pnumber']) && isset($_POST['password']) && ( isset($_POST['event']) || isset($_POST['activity']) ) ){
+        if( isset($_POST['fname']) &&  isset($_POST['email']) &&  isset($_POST['pnumber']) && isset($_POST['password'])){
             $fname = $_POST['fname'];
             $email = $_POST['email'];
             $pnumber = $_POST['pnumber'];
             $password = $_POST['password'];
-        
-            $event[] = $_POST['events'];
-            $activity[] = $_POST['activity'];
+            if(isset($_POST['events'])){
+                $event[] = $_POST['events'];
+            }
+            if(isset($_POST['activity'])){
+                $activity[] = $_POST['activity'];
+            }
+            echo "fuck";
+        }
+        else{
+            echo "hi";
         }
     }
 
