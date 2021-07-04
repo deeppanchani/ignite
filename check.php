2c4cf7efa3287c6b349e447826785e20bf79e85c
Deep Panchani <dlplearning00@gmail.com>
2021-07-04 14:29:38 +0530

    Updated isset() conditions

--- check.php
+++ check.php
@@ -1,6 +1,6 @@
 <?php
     if(isset($_POST['submit'])){
-        if(isset($_POST['fname']) &&  isset($_POST['email']) &&  isset($_POST['pnumber']) && isset($_POST['password'])){
+        if( isset($_POST['fname']) &&  isset($_POST['email']) &&  isset($_POST['pnumber']) && isset($_POST['password']) && ( isset($_POST['event']) || isset($_POST['activity']) ) ){
             $fname = $_POST['fname'];
             $email = $_POST['email'];
             $pnumber = $_POST['pnumber'];
